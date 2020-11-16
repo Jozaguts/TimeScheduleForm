1d3fd3eb43f18db3d225992774e37a7d46f1e5cb
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-11-16 16:58:18 -0600

    main schedule component, this one compact all of the secondary components it's a form with rows and cols for separation and make it responsive

---
+++ components/Forms/Schedule.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-form>
+    <v-row>
+      <v-col cols="12">
+        <TypeScheduleRadioButton :pName="pName" :pCanModify="pCanModify" />
+      </v-col>
+      <v-col cols="12">
+
+      </v-col>
+    </v-row>
+
+  </v-form>
+</template>
+
+<script>
+import  TypeScheduleRadioButton from '@/components/TypeScheduleRadioButton'
+export default {
+  components:{TypeScheduleRadioButton},
+name: "Schedule",
+  props:{
+    id:{
+      required:false,
+      default:null,
+    },
+    pEnabled:{
+      required:false,
+      default:true
+    },
+    pRemarks:{
+      required:false,
+      default: ''
+    },
+    p24hs:{
+      required:false,
+      default:false
+    },
+    pFirstDay:{
+      required:false,
+      default:0
+    },
+    pName:{
+      required:false,
+      default:''
+    },
+    pRadioLabels:{
+      type:Object,
+      required:false,
+      default: function() {
+        return {
+          alwaysOpen:{
+            label:'always open',
+            hint: 'parks, beaches, streets',
+            value: 'always_open'
+          },
+          selectedHours:{
+            label:'open on selected hours',
+            hint: 'input your own hours',
+            value:'selected_hours'
+          }
+        }
+      }
+    },
+    pCanModify:{
+      required:true,
+      type:Boolean,
+    },
+    pSchedule:{
+      type:Object,
+      required:true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
